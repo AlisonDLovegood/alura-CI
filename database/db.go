18c9129e3eae42ee740de90ae3c27214818e359b
AlisonDLovegood <alisondlovegood@gmail.com>
2024-10-27 20:41:12 -0300

    update - fix DB secrets

--- database/db.go
+++ database/db.go
@@ -16,7 +16,7 @@ var (
 func ConectaComBancoDeDados() {
 	host := os.Getenv("DB_HOST")
 	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PSWD")
+	password := os.Getenv("DB_PASSWORD")
 	dbname := os.Getenv("DB_NAME")
 	port := os.Getenv("DB_PORT")
 	stringDeConexao := "host=" + host + " user=" + user + " password=" + password + " dbname=" + dbname + " port=" + port + " sslmode=disable"
